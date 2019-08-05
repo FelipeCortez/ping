59caf477ac5fa99ec9b3f6cb9bea6f64c9788db9
Felipe Cortez <felipecortezfi@gmail.com>
2019-08-05 20:40:58 -0300

    There is a ball and it moves slowly

--- main.lua
+++ main.lua
@@ -1,3 +1,11 @@
+function love.load()
+  y = 200.0
+end
+
 function love.draw()
-  love.graphics.print("Hello World", 400, 300)
+  love.graphics.circle("line", 200, y, 10)
+end
+
+function love.update(dt)
+  y = y + (dt * 15)
 end
