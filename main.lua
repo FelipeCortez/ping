32d722f5757a1e1e641e41eed84c658db0d9305a
Felipe Cortez <felipecortezfi@gmail.com>
2019-08-10 08:30:41 -0300

    Clean up intersection code

--- main.lua
+++ main.lua
@@ -1,38 +1,26 @@
-function intersect(x1, x2, y1, y2, cx, cy, cr)
-  x1 = x1 - cx
-  x2 = x2 - cx
-  y1 = y1 - cy
-  y2 = y2 - cy
-  dx = x2 - x1
-  dy = y2 - y1
-  dr_squared = dx ^ 2 + dy ^ 2
-  D = x1 * y2 - x2 * y1
-  return cr ^ 2 * dr_squared > D ^ 2
-end
-
-function intersect2(ax, bx, ay, by, cx, cy, r)
-  ax = ax - cx;
-  ay = ay - cy;
-  bx = bx - cx;
-  by = by - cy;
-  a = (bx - ax)^2 + (by - ay)^2;
-  b = 2*(ax*(bx - ax) + ay*(by - ay));
-  c = ax^2 + ay^2 - r^2;
-  disc = b^2 - 4*a*c;
+function intersect(ax, bx, ay, by, cx, cy, r)
+  ax = ax - cx
+  ay = ay - cy
+  bx = bx - cx
+  by = by - cy
+  a = (bx - ax)^2 + (by - ay)^2
+  b = 2*(ax*(bx - ax) + ay*(by - ay))
+  c = ax^2 + ay^2 - r^2
+  disc = b^2 - 4*a*c
 
   if disc <= 0 then
-    return false;
+    return false
   end
 
-  sqrtdisc = math.sqrt(disc);
-  t1 = (-b + sqrtdisc)/(2*a);
-  t2 = (-b - sqrtdisc)/(2*a);
+  sqrtdisc = math.sqrt(disc)
+  t1 = (-b + sqrtdisc)/(2*a)
+  t2 = (-b - sqrtdisc)/(2*a)
 
   if (0 < t1 and t1 < 1) or (0 < t2 and t2 < 1) then
-    return true;
+    return true
   end
 
-  return false;
+  return false
 end
 
 function reflect(vx, vy, x1, x2, y1, y2)
@@ -58,7 +46,7 @@ function love.update(dt)
 end
 
 function love.draw()
-  if intersect2(linex1, linex2, liney1, liney2, ballx, bally, ballr) then
+  if intersect(linex1, linex2, liney1, liney2, ballx, bally, ballr) then
     love.graphics.clear(0, 0, 0)
   else
     love.graphics.clear(0.5, 0, 0)
@@ -67,7 +55,7 @@ function love.draw()
   for x=1,800,10 do
     for y=1,800,10 do
 
-      if intersect2(linex1, linex2, liney1, liney2, x, y, ballr) then
+      if intersect(linex1, linex2, liney1, liney2, x, y, ballr) then
         love.graphics.setColor(1, 1, 1)
       else
         love.graphics.setColor(1, 0, 0)
